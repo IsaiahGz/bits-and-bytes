25c0fb3392d5c9526c2dbd4b5904de7f925c0998
IsaiahGz <isaiahgala99@gmail.com>
2023-04-17 13:35:59 -0400

    Setup api/blog routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const blogRouts = require('./blog-routes');
 
 // Routes in this file are prepended with '/api'
 
 // Routing that handles CRUD operations for users
 router.use('/user', userRoutes);
+// Routing that handles CRUD operations for blog posts
+router.use('/blog', blogRouts);
 
 module.exports = router;
