55342bad9a1a4bead95aa77b5e652cafc7fac331
IsaiahGz <isaiahgala99@gmail.com>
2023-04-17 18:50:00 -0400

    Setup user routes and get routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,17 +1,57 @@
 const router = require('express').Router();
-
+const { User } = require('../../models/User');
 // Routes in this file are prepended with '/api/user'
 
 // Route to create a new user
 router.post('/', async (req, res) => {
-	// TODO
-	res.sendStatus(501);
+	try {
+		const userData = await User.create({
+			username: req.body.username,
+			password: req.body.password,
+			email: req.body.email,
+		});
+
+		// Save userId and loggedIn status to the session
+		req.session.userId = userData.id;
+		req.session.loggedIn = true;
+		req.session.save((err) => {
+			if (err) res.status(500).json(error);
+			else res.status(200).json(userData);
+		});
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 // Route to login
 router.post('/login', async (req, res) => {
-	// TODO
-	res.sendStatus(501);
+	try {
+		const userData = await User.findOne({ where: { email: req.body.email } });
+
+		if (!userData) {
+			// If there is no user in the database with the provided email, respond with an error message
+			res.status(400).json({ message: 'Incorrect email or password, please try again' });
+			return;
+		}
+
+		// Found the user, now check the password
+		const validPassword = await userData.checkPassword(req.body.password);
+		if (!validPassword) {
+			// If the password is invalid, respond with an error message
+			res.status(400).json({ message: 'Incorrect email or password, please try again' });
+			return;
+		}
+
+		// Save userId and loggedIn status to the session
+		req.session.userId = userData.id;
+		req.session.loggedIn = true;
+		req.session.save((err) => {
+			if (err) res.status(500).json(error);
+			else res.status(200).json({ user: userData, message: 'You are now logged in!' });
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 // Route to logout
@@ -21,7 +61,8 @@ router.post('/logout', async (req, res) => {
 			res.status(204).end();
 		});
 	} else {
-		res.status(404).end();
+		// If there is no user logged in, respond with 400 status
+		res.status(400).end();
 	}
 });
 
--- controllers/html-page-routes.js
+++ controllers/html-page-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const Blog = require('../models/Blog');
 // Routes in this file are prepended with '/'
 
 // GET request to render the homepage
@@ -15,6 +15,38 @@ router.get('/login', (req, res) => {
 	}
 
 	res.render('login');
+});
+
+// GET request to render a single blog post. If no blog post is found with the provided id, respond with a 404 status code
+router.get('/blog/:blogId', async (req, res) => {
+	try {
+		const blogData = await Blog.findByPk(req.params.blogId);
+		if (!blogData) {
+			res.status(404).json({ message: 'No blog post found with this id' });
+			return;
+		}
+		res.render('blog', { blogData });
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+router.get('/blog/new', (req, res) => {
+	// TODO: Must be logged in to create a new blog post/view this page
+	res.render('new-blog');
+});
+
+router.get('/blog/edit/:blogId', async (req, res) => {
+	try {
+		const blogData = await Blog.findByPk(req.params.blogId);
+		if (!blogData) {
+			res.status(404).json({ message: 'No blog post found with this id' });
+			return;
+		}
+		res.render('edit-blog', { blogData });
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 module.exports = router;
