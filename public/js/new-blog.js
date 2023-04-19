4ad3e602558d227210a1c4f7959fd84f00511f54
James Saint Paul <jamessp123@gmail.com>
2023-04-19 16:39:05 -0400

    updated submit button

--- public/js/new-blog.js
+++ public/js/new-blog.js
@@ -1,12 +1,12 @@
 document.getElementById('submit-button').addEventListener('click', async function (event) {
 	event.preventDefault();
-const title = document.getElementById('title')value;
-const content = document.getElementById('content')value;
-const response = await fetch('/api/blog', {
-	method: 'POST',
-	body: JSON.stringify({ title, content }),
-	headers: { 'Content-Type': 'application/json' },
-});
-const responseJSON = await response.json()
-document.location.replace('/blog', responseJSON.id);
+	const title = document.getElementById('blog-title').value;
+	const content = document.getElementById('content').value;
+	const response = await fetch('/api/blog', {
+		method: 'POST',
+		body: JSON.stringify({ title, content }),
+		headers: { 'Content-Type': 'application/json' },
+	});
+	const responseJSON = await response.json();
+	document.location.replace('/blog', responseJSON.id);
 });
