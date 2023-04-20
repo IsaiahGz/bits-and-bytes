9f3f08b97b7a7a63cbb0552850ddff0e11891e9e
IsaiahGz <isaiahgala99@gmail.com>
2023-04-19 20:49:26 -0400

    Fixed error in login page

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,19 @@
 const logoutButtonHandler = async (event) => {
+	// Send a POST request to the API endpoint
+	const response = await fetch('/api/user/logout', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+	});
 
-		// Send a POST request to the API endpoint
-		const response = await fetch('/api/user/logout', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-		});
+	if (response.ok) {
+		// If successful, redirect the browser to the homepage
+		document.location.replace('/');
+	} else {
+		console.log('Error logging out');
+	}
+};
 
-		if (response.ok) {
-			// If successful, redirect the browser to the homepage
-			document.location.replace('/');
-		} else {
-            console.log('Error logging out')
-		}
-	};
-
- document.querySelector('#logout-button').addEventListener('click', logoutButtonHandler);
+// Check if logout button exists and add event listener
+if (document.querySelector('#logout-button')) {
+	document.querySelector('#logout-button').addEventListener('click', logoutButtonHandler);
+}
